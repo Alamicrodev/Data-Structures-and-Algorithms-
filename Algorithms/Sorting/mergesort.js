61ecd33b08dd707457eb138d52988576adf4eadf
Alamicrodev <mohidalamsohail22@live.com>
2022-05-23 05:48:39 +0300

    Added Quick Sort and Merge Sort and moved sorting algorithms to their own folder

---
+++ Algorithms/Sorting/mergesort.js
@@ -0,0 +1,76 @@
+//  merge sort uses the technique of divide and conquer aswell;
+// suppose we have an array of videogame characters with their power level, now we sort them on the bases of highest to lowest.
+let characters = [40, 24, 53, 46, 66, 72, 95, 90];
+
+// ------------ MERGE FUNCTION ------------------
+// return to merge after going through merge sort, DON'T START HERE
+// merge function sorts the elements of two sorted subarrays(that came from the last recusion level) and merges them.
+function merge(array, start, mid, end) {
+	// i is the starting point of first sub array and j is the starting point of the second subarray.
+	let i = start;
+	let j = mid + 1;
+
+	// creating a new copy empty array.
+	let copyArr = [];
+
+	// while i and j both have not crossed their end
+	while (i <= mid && j <= end) {
+		// compare the value of one subarray elemet to the other
+		if (array[i] > array[j]) {
+			// push(add) the bigger one to the new array
+			copyArr.push(array[i]);
+			// increment the pointer in the subarray where you pushed it from
+			i++;
+		} else {
+			// incase of opposite the other subarray has the bigger value so we puch the other one and move pointer.
+			copyArr.push(array[j]);
+			j++;
+		}
+	}
+
+	// if the above loop ends this means any one of the subarrays has ended.
+	// however the second subarray might still have elements left
+
+	// checking which one ended, then pushing the remaining elements of the otherone.
+	// remember both the subarrays are already sorted from last recusion level so you can push the remaining elements they are sorted.
+	if (j > end) {
+		copyArr.push(...array.slice(i, mid + 1));
+	} else if (i > mid) {
+		copyArr.push(...array.slice(j, end + 1));
+	}
+
+	// now we replace the orignal array's elements with the copy array in the index positions provided.
+	for (let k = start, p = 0; k <= end, p <= copyArr.length - 1; k++, p++) {
+		array[k] = copyArr[p];
+	}
+}
+// -------------- END OF MERGE ----------------
+
+// ------------- MERGE SORT -------------------
+// merge sort function intakes the array, the starting in idex and the ending index.
+// the start and end represent the sub array we will work with.
+function mergeSort(array, start, end) {
+	// if the start is less than end that means the subarray has more than one characters
+	if (start < end) {
+		// we create the mid point of the array/subarray.
+		let midpoint = Math.floor((start + end) / 2);
+
+		// we call the same function recursively, with the two pieces(subarrays) separated from midpoint.
+		mergeSort(array, start, midpoint);
+		mergeSort(array, midpoint + 1, end);
+
+		// now we send the mid point, start and end to the merge function.
+		merge(array, start, midpoint, end);
+	}
+
+	// if it has one element we do nothing LOL.
+}
+
+// calling merge sort on our characters
+mergeSort(characters, 0, characters.length - 1);
+
+// printing sorted characters
+console.log(characters);
+
+// Time Complexity: O(n log n)
+// Space Complexity: O(n)
---
+++ Algorithms/Sorting/quicksort.js
@@ -0,0 +1,71 @@
+// In Quicksort we resort to recursive technique of dividing and conquering.
+// In this method we choose a pivot
+// Pivot: random number chosen from array which goes in the middle with greater and smaller value subarrays in each side.
+
+// imagine a list of book stacks in a market's cupboards. Yeah I know its useless example but here we are.
+let bookStack = [5, 2, 1, 15, 30, 3, 9, 4];
+
+// ------------------------- Partition Start -----------------------------------
+// return to partitition after checking out quicksort. DON'T START HERE
+function Partition(array, first, last) {
+	// You can have any element as pivot value but its easier to have the last one as pivot.
+	let pivotValue = array[last];
+
+	//  Pivot index keeps track of the first smaller/bigger value(the values that go on the right of pivot depending on type decending or accending), in the end allowing us to move pivot to the center, it always starts from the first index position of the subarray.
+	// eg [30,15,9,5,3(index position),2,1, 4(pivot)] in the end :p
+	let pivotIndex = first;
+
+	// going through all elements of the subarray, except the last one which is the pivot.
+	for (let i = first; i <= last - 1; i++) {
+		// if the value(i) is greater than pivot value, we swap it with pivot index and increment pivot index.
+		// this moves all bigger values to the left and smaller to the right.
+		if (array[i] > pivotValue) {
+			[array[i], array[pivotIndex]] = [array[pivotIndex], array[i]];
+			pivotIndex++;
+		}
+	}
+
+	// at this point pivot index stores the index position of first smaller element from pivot.
+	// eg [30,15,9,5,3(index position),2,1, 4(pivot)]
+
+	// so we swap it with pivot resulting in pivot in the mid, smaller and larger values on each side.
+	[array[last], array[pivotIndex]] = [array[pivotIndex], array[last]];
+
+	// now we return pivot index.
+	return pivotIndex;
+}
+// ---------------------- Partition End ----------------------------
+
+//  --------------------  Quick Sort Start ------------------------
+// Quicksort intakes an array, the starting index and the ending index.
+// The array passed in during recursion is always the same, we just change the indexes to know which subsection of the array to work with.
+function quickSort(
+	someArray,
+	firstindex = 0,
+	lastIndex = someArray.length - 1
+) {
+	// if the given indexes are the same number this means we are only working with one element or none, so we return. This is the break out point of recursion.
+	if (firstindex >= lastIndex) {
+		return;
+		// why return nothing? remember an array passed in function always points to same memory address so if we make changes in functions the changes apply to orignal array so we don't have to return anything.
+	}
+
+	// Partition function returns the index position of the pivot
+	// Pivot: random number chosen from array which goes in the middle with greater and smaller value subarrays in each side.
+	let pivotIndex = Partition(someArray, firstindex, lastIndex);
+
+	// This allows us to call the function again with 2 different parts before and after the pivot. Here's recursion :)))
+	// Notice that we still pass the same array.
+	quickSort(someArray, 0, pivotIndex - 1);
+	quickSort(someArray, pivotIndex + 1, lastIndex);
+}
+// --------------------------- Quick Sort End -----------------------------------
+
+// Calling quicksort on our array.
+quickSort(bookStack, 0, bookStack.length - 1);
+
+// Printing to see if it works
+console.log(bookStack);
+
+// Time Complexity: O(n log n) Worst: O(n^2)
+// Space Complexity: O(log n)
